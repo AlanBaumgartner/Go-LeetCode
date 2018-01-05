09c7228f9ee620f5b1c35c72e16ef84d1effe5e1
Alan Baumgartner <alanvbaumgartner@gmail.com>
2018-01-05 04:36:42 -0600

    Cleaned up reverse integer.

--- Reverse-Integer/Solution.go
+++ Reverse-Integer/Solution.go
@@ -2,21 +2,14 @@ package reverse
 
 func reverse(x int) int {
 	ret := 0
-	if x > 0 {
-		for x > 0 {
-			remainder := x % 10
-			ret *= 10
-			ret += remainder
-			x /= 10
-		}
-	} else {
-		for x < 0 {
-			remainder := x % 10
-			ret *= 10
-			ret += remainder
-			x /= 10
-		}
+
+	for x != 0 {
+		remainder := x % 10
+		ret *= 10
+		ret += remainder
+		x /= 10
 	}
+
 	if ret >= 2147483647 || ret <= -2147483648 {
 		return 0
 	}
